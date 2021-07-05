25eb2d12a74f5fdd0eaa9cf0bfa912e7ff9721d7
Fulano de Tal <jefferson.jsm3444@gmail.com>
2021-07-05 19:59:53 -0300

    tudo ok hj

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { 
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image
 } from 'react-native';
-
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import userImg from '../assets/jefferson.png'; 
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 export function Header(){
     return (
         <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text style={styles.userName}>Jefferson</Text>
+            </View>
+
+            <Image source={userImg} style={styles.image}/>
 
         </View>
     )
@@ -17,5 +27,26 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+        padding: 20
+    },
+    image: {
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    },
+    greeting: {
+        fontSize: 26,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 26,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40
     }
 })
